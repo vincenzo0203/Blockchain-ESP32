f5269fd75e6109528a9f430dd3847e4c44f1d55f
Fradiale1 <francesco.adinolfi02@gmail.com>
2024-11-18 12:22:45 +0100

    Add information ESP32 on file readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Blockchain-ESP32
+
+## ESP32
+La scheda che è stata usata per la realizzazione di questo progetto è la ESP32-WROOM-32E con la seguente configurazione dei pin
+![BLOCKCHAIN-ESP32](esp32_pinout.png)
+
+### Installazione ambiente e librerie
+- Installare Arduino IDE
+- Scaricare le librerie:
+    - MFRC522 (by Github Developer v.1.4.11)
+    - LiquidCrystal (by Arduino Adafruit v.1.0.7)
+
+### Modifica della libreria MFRC522
+Per poter usare la libreria MFRC522 con la ESP32 bisogna modificare il file MFRC522Extended.cpp situato nel percorso file Documents\Arduino\libraries\MFRC522\src\MFRC522Extendend.cpp. Bisogna cambiare due righe: 824 e 847.
+- rimpiazzare: if (backData && (backlen>0))
+- con: if (backData && backlen != nullptr)
---
+++ esp32/Controllo_accessi_esp32_con_display_e_buzzer/Controllo_accessi_esp32_con_display_e_buzzer.ino
@@ -0,0 +1,80 @@
+#include <MFRC522.h>
+#include <LiquidCrystal.h>
+
+/**
+* Source code:
+* https://www.italiantechproject.it/sensori-con-arduino/lettore-rfid
+* For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-rfid-nfc
+* For more detail Lab 14 (esp32 lcd 16x2 without I2C https://deepbluembedded.com/esp32-lcd-display-16x2-without-i2c-arduino/?utm_content=cmp-true)
+*/
+
+#define GREEN_LED_PIN 32
+#define RED_LED_PIN 4
+
+//per modulo RFID
+#define SS_PIN  5  // ESP32 pin GPIO5 
+#define RST_PIN 27 // ESP32 pin GPIO27
+ 
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+LiquidCrystal My_LCD(14, 13, 17, 26, 25, 33);  // RS, EN, D4, D5, D6, D7
+ 
+String users[] = {"f3620c35"};
+int usersSize = sizeof(users)/sizeof(String);
+
+int buzzerPin = 16;
+ 
+void setup(){
+  SPI.begin();
+  rfid.PCD_Init();
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+  My_LCD.begin(16, 2);
+  My_LCD.print("AAAAA");
+}
+ 
+void loop(){
+  if(rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()){
+    String uid = getUID();
+    if(checkUID(uid)){
+      blinkLed(GREEN_LED_PIN, 2000, 1);
+      My_LCD.print("Accesso consentito");
+    }else{
+      blinkLed(RED_LED_PIN, 400, 2);
+      My_LCD.print("Accesso negato");
+      tone(buzzerPin, 1000, 500);
+    }
+  }
+  delay(10);
+  My_LCD.clear();
+}
+ 
+String getUID(){
+  String uid = "";
+  for(int i = 0; i < rfid.uid.size; i++){
+    uid += rfid.uid.uidByte[i] < 0x10 ? "0" : "";
+    uid += String(rfid.uid.uidByte[i], HEX);
+  }
+  rfid.PICC_HaltA();
+  return uid;
+}
+ 
+bool checkUID(String uid){
+  for(int i = 0; i < usersSize; i++){
+    if(users[i] == uid){
+      return true;
+    }
+  }
+  return false;
+}
+ 
+void blinkLed(int led, int duration, int repeat){
+  for(int i = 0; i < repeat; i++){
+    digitalWrite(led, HIGH);
+    delay(duration/2);
+    digitalWrite(led, LOW);
+    delay(duration/2);
+  }
+}
