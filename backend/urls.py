aa5436aeb22200431ad4b410528c3a75b0a96289
vincenzo0203 <v.cerciello1@studenti.unisa.it>
2025-01-31 10:36:02 +0100

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from accounts import views
 
 urlpatterns = [
+    path('', views.login_view, name='home'),
     path('blockchain/', include('blockchain.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
